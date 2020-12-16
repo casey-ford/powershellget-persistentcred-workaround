192e88223201cf4488f2923c910c7a4aa0073403
Casey Ford <casey.ford@relativity.com>
2020-12-15 21:31:25 -0500

    removing set-defaultparamtervalue file from source

--- Set-DefaultParameterValue.ps1
+++
@@ -1,6 +0,0 @@
-#Import Credential
-$Credential = Import-Clixml -Path "${env:\USERPROFILE}\jfrog.xml"
-$PSDefaultParameterValues = @{
-    "Find-Module:Credential"=$Credential;
-    "Register-PSRepository:Credential"=$Credential
-}
